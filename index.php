bf272ce813e1edfad092374eb61469f0a1cf7a6f
Jessica Sestak <jsestak2@mail.greenriver.edu>
2021-02-19 23:39:49 -0500

    Added data layer

--- index.php
+++ index.php
@@ -6,7 +6,10 @@ error_reporting(E_ALL);
 
 //Require the autoload file
 require_once('vendor/autoload.php');
+require_once('model/data-layer.php');
 
+//Start a session
+session_start();
 //Create an instance of the Base class
 $f3 = Base::instance();
 $f3->set('DEBUG', 3);
@@ -15,6 +18,35 @@ $f3->route('GET /', function() {
 
     $view = new Template();
     echo $view->render('views/home.html');
+});
+
+$f3->route('GET|POST /survey', function($f3){
+
+    //if it doesnt work with conditional
+    $f3->set('midterm', getSurveyQuestions());
+
+    $view = new Template();
+    echo $view->render('views/survey.html');
+});
+
+$f3->route('POST /summary', function(){
+    //add data from form 2 to session array
+    if(isset($_POST['petName'])){
+        $_SESSION['petName'] = $_POST['petName'];
+    }
+
+    if(isset($_POST['size'])){
+        $_SESSION['size'] = $_POST['size'];
+    }
+
+    if(isset($_POST['accessory'])) {
+        $_SESSION['accessory'] = implode(", ", $_POST['accessory']);
+    }
+
+    //display a view
+    $view = new Template();
+    echo $view->render('views/summary.html');
+    //var_dump($_POST);
 });
 
 
---
+++ model/data-layer.php
@@ -0,0 +1,5 @@
+<?php
+function getSurveyQuestions() {
+return array("This is so long!", "Will i finish?", "I am worried!");
+
+}
